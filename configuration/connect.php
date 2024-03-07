20cb55b75cc1157fec9c6b282fc6375a021a0c64
MauMau221 <mauriciobgsp@gmail.com>
2024-03-07 10:03:12 -0300

    Iniciando Conexão com bd

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,1 +1,31 @@
 <!--Conexão com banco de dados -->
+
+<?php 
+
+    define('HOST','localhost');
+    define('DATABASENAME','crud-mvc-poo-php');
+    define('USER','root');
+    define('PASSWORD','root');
+
+    class Connect{
+        protected $connection;
+
+        function __construct()
+        {
+            $this->connectDatabase();
+        }
+
+        function connectDatabase(){
+            try
+            {
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
+            }
+            catch (PDOException $e)
+            {
+                echo "ERRO".$e->getMessage();
+            }
+        }
+    }
+    
+
+?>
