8cfdb49c782f91787ece7ea61aa0fe82b3417e86
MauMau221 <mauriciobgsp@gmail.com>
2024-03-08 00:34:08 -0300

    exibindo dados do banco

--- controllers/clientsController.php
+++ controllers/clientsController.php
@@ -16,7 +16,7 @@
         function getAll()
         {
             $resultData = $this->model->getAll();
-            print_r($resultData);
+            require_once('./views/index.php');
         }
     }
 ?>
--- views/index.php
+++ views/index.php
@@ -1,1 +1,34 @@
 <!-- Listagem de Registros -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clients</title>
+</head>
+<body>
+    <h1>Clients List</h1>
+    <div class="content">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                </tr>
+            </thead>
+        </table>
+        <tbody>
+            <?php foreach($resultData as $data): ?>
+                <tr>
+                    <td><?php echo $data['id'] ?></td>
+                    <td><?php echo $data['name'] ?></td>
+                    <td><?php echo $data['email'] ?></td>
+                    <td><?php echo $data['phone'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </div>
+</body>
+</html>
